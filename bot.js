cdd7c39027e344671f666d42ab1d927c8b35eb56
6Fe6l <68379150+6Fe6l@users.noreply.github.com>
2020-07-17 10:34:19 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,278 @@
+const Discord = require("discord.js");
+const myid = ['569861608344518660'];
+const prefix = ['.'];
+const cmd = require("node-cmd")
+const client = new Discord.Client();
+const moment = require("moment");
+const fs = require("fs");
+
+
+
+
+
+
+client.login(process.env.TOKEN);
+
+
+
+         client.on('message', message => {
+    if (message.content.startsWith(".av")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+         
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+client.on('message', message => {
+    if(message.author.bot || message.channel.type == "dm") return undefined;
+    if(message.content.startsWith(prefix+'tag')){
+            if(message.author.id != 569861608344518660)return message.reply('Only '+client.users.get("569861608344518660")+' can use this command.')
+
+        var args1 = message.content.split(" ").slice(1);
+        var args = args1[0];
+        
+        if(!args){
+            client.users.forEach(user => {
+            if(user.discriminator === message.author.discriminator && user !== message.author){
+                message.channel.send(`${user.username}#${user.discriminator}`)
+            }else if(user.discriminator === message.author.discriminator && user == message.author){
+                message.channel.send("مابعرف شو بدي اقلك يا اخوي بس..").then(m => {
+          setTimeout(async function() {
+            return m.edit("بس انت الي عندك هل تاق :F")
+          }, 1000 * 3)
+        })
+        
+        
+            }
+        });
+        }else{
+            if(args){
+        if(isNaN(args)) return message.channel.send('بس ارقام يا حبيبي يا قلبي')
+                client.users.forEach(user => {
+                    try{
+            if(user.discriminator === args){
+                message.channel.send(`${user.username}#${user.discriminator}`)
+            }
+            }catch(e){
+                return message.reply('مافي حدا هيك تاقو')
+            }
+        });
+            }
+        }
+    }
+});
+
+
+
+
+client.on('message', message => { 
+           if (message.content.startsWith(prefix + "id")) {
+     var args = message.content.split(" ").slice(1);
+     let user = message.mentions.users.first();
+     var men = message.mentions.users.first();
+        var heg;
+        if(men) {
+            heg = men
+        } else {
+            heg = message.author
+        }
+      var mentionned = message.mentions.members.first();
+         var h;
+        if(mentionned) {
+            h = mentionned
+        } else {
+            h = message.member
+        }
+               moment.locale('ar-TN');
+      var id = new  Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL) 
+    .setColor("#707070")
+    .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
+    .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
+    .setFooter(`Probot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
+    .setThumbnail(heg.avatarURL);
+    message.channel.send(id)
+}       });
+
+
+
+
+
+
+
+
+client.on("message", message => {
+  let command = message.content.split(" ")[0];
+  if (command == prefix + "info") {
+    var server = client.guilds.find(
+      c => c.id === message.content.split(" ")[1]
+    );//by ${ ! YamanSaleh }| Myame [🍇]#1282
+    if (!server)
+      return message.channel.send("**I Can't find this server :x:**");
+    message.channel.send(
+      new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle(`📖 **${server.name}** Info`)
+        .setImage(
+          `https://cdn.discordapp.com/icons/${server.id}/${server.icon}.png?size=1024`
+        )//by ${ ! YamanSaleh }| Myame [🍇]#1282
+        .addField(
+          "**Members Cout:**",
+          `**${server.memberCount -
+            server.members.filter(m => m.user.bot).size}** | **${
+            server.members.filter(m => m.user.bot).size
+          }** bots`,
+          true
+        )
+        .addField(
+          `**Channels [${server.channels.size}]**`,
+          `**${
+            server.channels.filter(m => m.type === "text").size
+          }** Text | **${
+            server.channels.filter(m => m.type === "voice").size
+          }** Voice | **${
+            server.channels.filter(m => m.type === "category").size
+          }** Category`,
+          true
+        )
+        .addField("**Server Region:**", server.region, true)
+        .addField("**Server Owner**", `**${server.owner}**`, true)
+        .addField(`**Roles Count [${server.roles.size}]**`, `** **`, true)
+        .addField(
+          `**verification Level [ ${server.verificationLevel} ]**`,
+          `** **`,
+          true
+        )
+    );//by ${ ! YamanSaleh }| Myame [🍇]#1282
+  }
+});//alpha-codes
+
+
+
+client.on('message', message => {
+var args = message.content.split(" ").slice(1);    
+if(message.content.startsWith(prefix + 'user')) {
+var year = message.author.createdAt.getFullYear()
+var month = message.author.createdAt.getMonth()
+var day = message.author.createdAt.getDate()
+var men = message.mentions.users.first();  
+let args = message.content.split(' ').slice(1).join(' ');
+if (args == '') {
+var z = message.author;
+}else {
+var z = message.mentions.users.first();
+}
+
+let d = z.createdAt;          
+let n = d.toLocaleString();   
+let x;                       
+let y;                        
+
+if (z.presence.game !== null) {
+y = `${z.presence.game.name}`;
+} else {
+y = "No Playing...";
+}
+if (z.bot) {
+var w = 'BOT';
+}else {
+var w = 'MEMBER';
+}
+let embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle(`**INFO** ${z.username}`)
+.addField('`Your Name`',`**<@` + `${z.id}` + `>**`, true)
+.addField('`ID`', "**"+ `${z.id}` +"**",true)
+.addField('`Status`','**'+y+'**' , true)
+.addField('`Acount Type`',"**"+ w + "**",true)    
+.addField('`Your Tag`',"**#" +  `${z.discriminator}**`,true)
+.addField('`Your account created in`' ,year + "-"+ month +"-"+ day)    
+.addField("`Entered the server in`", message.member.joinedAt.toLocaleString())    
+.addField("`Last Message`", message.author.lastMessage)            
+.setThumbnail(`${z.avatarURL}`)
+.setFooter(message.author.username, message.author.avatarURL)
+
+message.channel.send({embed});
+    if (!message) return message.reply('**ضع المينشان بشكل صحيح  ? **')
+
+}
+});
+
+
+
+
+client.on('ready',async () => {
+console.log("Starting..");
+let g = client.guilds.get("729269534745427988"); // ايدي السيرفر
+let c = g.channels.get("730727923388121140");// ايدي الروم
+if(c.type === 'voice') {
+c.join();
+setInterval(() => {
+if(g.me.voiceChannel && g.me.voiceChannelID !== c.id || !g.me.voiceChannel) c.join();
+}, 1);
+} else {
+console.log('Failed To Join: \n The Channel Type isn "Listening."')
+}
+})
+
+
+
+
+
+
+
+client.on('message', message => {
+if (message.content.startsWith(prefix + "uptime")) {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+  
+
+    let uptime = client.uptime;
+
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+
+    while (notCompleted) {
+
+        if (uptime >= 8.64e+7) {
+
+            days++;
+            uptime -= 8.64e+7;
+
+        } else if (uptime >= 3.6e+6) {
+
+            hours++;
+            uptime -= 3.6e+6;
+
+        } else if (uptime >= 60000) {
+
+            minutes++;
+            uptime -= 60000;
+
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+
+        }
+
+        if (uptime < 1000)  notCompleted = false;
+
+    }
+
+    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
+
+
+}
+});
