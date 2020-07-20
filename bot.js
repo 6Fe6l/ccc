7bc5fe59b11285869856377e2c1c050c0a4e1f87
6Fe6l <68379150+6Fe6l@users.noreply.github.com>
2020-07-20 23:57:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const myid = ['287898437058560000'];
-const prefix = ['.'];
+const prefix = ['&'];
 const cmd = require("node-cmd")
 const client = new Discord.Client();
 const moment = require("moment");
@@ -16,7 +16,7 @@ client.login(process.env.TOKEN);
 
 
          client.on('message', message => {
-    if (message.content.startsWith(".av")) {
+    if (message.content.startsWith("&av")) {
         var mentionned = message.mentions.users.first();
     var x5bzm;
       if(mentionned){
@@ -33,44 +33,6 @@ client.login(process.env.TOKEN);
 });
 
 
-client.on('message', message => {
-    if(message.author.bot || message.channel.type == "dm") return undefined;
-    if(message.content.startsWith(prefix+'tag')){
-            if(message.author.id != 287898437058560000)return message.reply('Only '+client.users.get("287898437058560000")+' can use this command.')
-
-        var args1 = message.content.split(" ").slice(1);
-        var args = args1[0];
-        
-        if(!args){
-            client.users.forEach(user => {
-            if(user.discriminator === message.author.discriminator && user !== message.author){
-                message.channel.send(`${user.username}#${user.discriminator}`)
-            }else if(user.discriminator === message.author.discriminator && user == message.author){
-                message.channel.send("مابعرف شو بدي اقلك يا اخوي بس..").then(m => {
-          setTimeout(async function() {
-            return m.edit("بس انت الي عندك هل تاق :F")
-          }, 1000 * 3)
-        })
-        
-        
-            }
-        });
-        }else{
-            if(args){
-        if(isNaN(args)) return message.channel.send('بس ارقام يا حبيبي يا قلبي')
-                client.users.forEach(user => {
-                    try{
-            if(user.discriminator === args){
-                message.channel.send(`${user.username}#${user.discriminator}`)
-            }
-            }catch(e){
-                return message.reply('مافي حدا هيك تاقو')
-            }
-        });
-            }
-        }
-    }
-});
 
 
 
@@ -211,19 +173,6 @@ message.channel.send({embed});
 
 
 
-client.on('ready',async () => {
-console.log("Starting..");
-let g = client.guilds.get("729269534745427988"); // ايدي السيرفر
-let c = g.channels.get("730727923388121140");// ايدي الروم
-if(c.type === 'voice') {
-c.join();
-setInterval(() => {
-if(g.me.voiceChannel && g.me.voiceChannelID !== c.id || !g.me.voiceChannel) c.join();
-}, 1);
-} else {
-console.log('Failed To Join: \n The Channel Type isn "Listening."')
-}
-})
 
 
 
